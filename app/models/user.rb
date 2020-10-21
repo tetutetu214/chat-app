ccecbf6d49afc134ce24d314bf12f4d1fb13cd2c
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-10-21 17:16:23 +0900

    プルダウンの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  
-  has_many :room_users 
-  has_many :rooms, througth: :room_users
-   
+         :recoverable, :rememberable,  :validatable
   validates :name, presence: true
+
+  has_many :room_users
+  has_many :rooms, through: :room_users
 end
